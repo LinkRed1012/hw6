3db5c8b465cfc00f1ab212d559805e38ac7dfb84
LinkRed1012 <a422599@gmail.com>
2021-05-24 17:57:23 +0800

    fix hw5

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-import Connect  from './js/Controller/connect'
-
-ReactDOM.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  <Connect/>,
-  document.getElementById('root')
-);
+import TodoApp from './TodoApp.js';
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
